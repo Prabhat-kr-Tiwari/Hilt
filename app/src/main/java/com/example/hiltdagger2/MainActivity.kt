a71b2df77ea093b1574d7ec672b5253bd1c78622
Prabhat kumar tiwari <prabhat.mobilefirst@gmail.com>
2024-02-07 01:32:59 +0530

    Added the hilt modules and provides

--- app/src/main/java/com/example/hiltdagger2/MainActivity.kt
+++ app/src/main/java/com/example/hiltdagger2/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.hiltdagger2
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 
 @AndroidEntryPoint
@@ -16,29 +26,95 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println(someClass.doAThing())
-        println(someClass.doSomeOtherThing())
+
     }
 }
+
+//first issue of constructor injection with using the interface
+
 
 class SomeClass
 @Inject
 constructor(
-    private val someOtherClass: SomeOtherClass
-){
-    fun doAThing():String{
-        return "look i do a thing"
-    }
-
-    fun doSomeOtherThing():String{
-        return someOtherClass.doSomeOtherThing()
+    private val SomeInterfaceImpl: SomeInterface,
+    private val gson: Gson
+) {
+    fun doAThing(): String {
+        return "Look I got: ${SomeInterfaceImpl.getAThing()}"
     }
 }
 
-class SomeOtherClass
+class SomeInterfaceImpl
 @Inject
-constructor(){
-
-    fun doSomeOtherThing():String{
-        return "look Doing some other thing"
+constructor(
+    private val someDependency:String
+) : SomeInterface {
+    override fun getAThing(): String {
+        return "A Thing ${someDependency}"
     }
 }
+
+interface SomeInterface {
+    fun getAThing(): String
+}
+//solving the problem of creating object of
+/*
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModule{
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl:SomeInterfaceImpl
+    ):SomeInterface
+
+}*/
+
+
+//  if you insstall something in the activity component then scope must be activityscoped
+/*@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModule{
+    @ActivityScoped
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl:SomeInterfaceImpl
+    ):SomeInterface
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindGson(
+        gson:Gson
+    ):Gson
+
+}*/
+
+//using @Provides
+@InstallIn(SingletonComponent::class)
+@Module
+ class MyModule{
+
+    @Singleton
+    @Provides
+    fun provideSomeString():String{
+        return "Some string"
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideSomeInterface(
+        someString: String
+    ):SomeInterface{
+        return SomeInterfaceImpl(someString)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson():Gson{
+        return Gson()
+    }
+
+}
+
+
