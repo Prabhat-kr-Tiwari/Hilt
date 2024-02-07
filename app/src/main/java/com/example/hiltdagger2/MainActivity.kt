9d9703c7ad78093fae4a502a6a242f402b56bd30
Prabhat kumar tiwari <prabhat.mobilefirst@gmail.com>
2024-02-07 11:10:44 +0530

    Providing instance of same type

--- app/src/main/java/com/example/hiltdagger2/MainActivity.kt
+++ app/src/main/java/com/example/hiltdagger2/MainActivity.kt
@@ -1,44 +1,90 @@
 package com.example.hiltdagger2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
-
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-
     //field injection
     @Inject
     lateinit var someClass: SomeClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(someClass.doAThing())
-        println(someClass.doSomeOtherThing())
+        println(someClass.doAThing1())
+        println(someClass.doAThing2())
     }
 }
-
 class SomeClass
 @Inject
 constructor(
-    private val someOtherClass: SomeOtherClass
-){
-    fun doAThing():String{
-        return "look i do a thing"
-    }
+    @Impl1 private val SomeInterfaceImpl1: SomeInterface,
+    @Impl2 private val SomeInterfaceImpl2: SomeInterface
 
-    fun doSomeOtherThing():String{
-        return someOtherClass.doSomeOtherThing()
+) {
+    fun doAThing1(): String {
+        return "Look I got: ${SomeInterfaceImpl1.getAThing()}"
+    }
+    fun doAThing2(): String {
+        return "Look I got: ${SomeInterfaceImpl2.getAThing()}"
     }
 }
-
-class SomeOtherClass
+class SomeInterfaceImpl1
 @Inject
-constructor(){
+constructor(
 
-    fun doSomeOtherThing():String{
-        return "look Doing some other thing"
+) : SomeInterface {
+    override fun getAThing(): String {
+        return "A Thing1 "
     }
 }
+class SomeInterfaceImpl2
+@Inject
+constructor(
+
+) : SomeInterface {
+    override fun getAThing(): String {
+        return "A Thing2 "
+    }
+}
+interface SomeInterface {
+    fun getAThing(): String
+}
+@InstallIn(SingletonComponent::class)
+@Module
+class MyModule{
+
+    @Impl1
+    @Singleton
+    @Provides
+    fun provideSomeInterface1(
+        someString: String
+    ):SomeInterface{
+        return SomeInterfaceImpl1()
+    }
+
+    @Impl2
+    @Singleton
+    @Provides
+    fun provideSomeInterface2(
+        someString: String
+    ):SomeInterface{
+        return SomeInterfaceImpl2()
+    }
+}
+
+//difference between two
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl1
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl2
